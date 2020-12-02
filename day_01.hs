fd94a4905aaf6f90151d68273f27209b6451e15c
KuldeepSinh <chauhan.kuldeep.sinh@gmail.com>
2020-12-02 13:01:04 -0500

    Added new line after show.

--- day_01.hs
+++ day_01.hs
@@ -5,14 +5,14 @@ pairs :: [b] -> [(b, b)]
 pairs ls = [(x, y) | (x : ys) <- tails ls, y <- ys]
 
 solvePairs :: String -> String
-solvePairs = show . (\(a, b) -> a * b) . head . filter (\(a, b) -> a + b == 2020) . pairs . map read . words
+solvePairs = (++ "\n") . show . (\(a, b) -> a * b) . head . filter (\(a, b) -> a + b == 2020) . pairs . map read . words
 
 -- Puzzle - 02
 triples :: [c] -> [(c, c, c)]
 triples ls = [(x, y, z) | (x : ys) <- tails ls, (y : zs) <- tails ys, z <- zs]
 
 solveTriples :: String -> String
-solveTriples = show . (\(a, b, c) -> a * b * c) . head . filter (\(a, b, c) -> a + b + c == 2020) . triples . map read . words
+solveTriples = (++ "\n") . show . (\(a, b, c) -> a * b * c) . head . filter (\(a, b, c) -> a + b + c == 2020) . triples . map read . words
 
 -- main
 -- compile as follows "ghc --make day_01.hs"
