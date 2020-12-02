16392df209ca73aa5a8dad7d2dd306138d9b3ad1
KuldeepSinh <chauhan.kuldeep.sinh@gmail.com>
2020-12-01 21:43:25 -0500

    Added solutions for day 1 puzzles.

---
+++ day_01.hs
@@ -0,0 +1,22 @@
+import Data.List (tails)
+
+-- Puzzle - 01
+pairs :: [b] -> [(b, b)]
+pairs ls = [(x, y) | (x : ys) <- tails ls, y <- ys]
+
+solvePairs :: String -> String
+solvePairs = show . (\(a, b) -> a * b) . head . filter (\(a, b) -> a + b == 2020) . pairs . map read . words
+
+-- Puzzle - 02
+triples :: [c] -> [(c, c, c)]
+triples ls = [(x, y, z) | (x : ys) <- tails ls, (y : zs) <- tails ys, z <- zs]
+
+solveTriples :: String -> String
+solveTriples = show . (\(a, b, c) -> a * b * c) . head . filter (\(a, b, c) -> a + b + c == 2020) . triples . map read . words
+
+-- main
+-- compile as follows "ghc --make day_01.hs"
+-- copy input file with name = "day_01.input"
+-- run as follows "cat day_01.input | ./day_01"
+main :: IO ()
+main = interact $ solveTriples
