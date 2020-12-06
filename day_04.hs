d1c8a88beba297f32f5164bc9641b459e3614bbf
KuldeepSinh <chauhan.kuldeep.sinh@gmail.com>
2020-12-05 23:54:29 -0500

    extracted predicate for puzzle 1

--- day_04.hs
+++ day_04.hs
@@ -9,9 +9,12 @@ dropColon (x, (_ : ys)) = (x, ys)
 convertToKeyValuePairs :: [[String]] -> [[(String, String)]]
 convertToKeyValuePairs xs = [map (dropColon . break (== ':')) x | x <- xs]
 
+puz1Predicate :: [([Char], b)] -> Bool
+puz1Predicate = (\x -> (length x == 8 || (length x == 7 && fst (x !! 0) /= "cid") && (length x == 7 && fst (x !! 1) /= "cid")))
+
 -- puzzel 1
 countValidPassports :: String -> Int
-countValidPassports = length . filter (\x -> (length x == 8 || (length x == 7 && fst (x !! 0) /= "cid") && (length x == 7 && fst (x !! 1) /= "cid"))) . convertToKeyValuePairs . normalize
+countValidPassports = length . filter puz1Predicate . convertToKeyValuePairs . normalize
 
 main :: IO ()
 main = interact $ (++ "\n") . show . countValidPassports
