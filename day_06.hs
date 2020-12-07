81393a74499a32cf16fb0ccb879ae87fdcf17e6c
KuldeepSinh <chauhan.kuldeep.sinh@gmail.com>
2020-12-06 22:53:32 -0500

    refactored solution 1

--- day_06.hs
+++ day_06.hs
@@ -1,9 +1,9 @@
-import Data.List (groupBy, intersect)
+import Data.List (groupBy, intersect, union)
 import qualified Data.Set as Set
 
 -- puzzle 1
 main :: IO ()
-main = interact $ (++ "\n") . show . sum . map (length . Set.fromList . concat) . groupBy (\x y -> and [x /= "", y /= ""]) . lines
+main = interact $ (++ "\n") . show . sum . map (length . foldr1 union) . groupBy (\x y -> and [x /= "", y /= ""]) . lines
 
 -- puzzle 2
 -- main :: IO ()
